8618d0fe0146c8e690e6463925ca5249a6df93f8
Malcome Fein <mfein90@gmail.com>
2018-08-03 19:36:39 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,7 +5,7 @@ def get_first_name_of_season_winner(data, season)
   #binding.pry
     data.each do |show_season_number, contestants_data|
       binding.pry
-      if contestants_data
+      if contestants_data == win
     end
     end
 end
