195548e3909f8afaf49ccbcdaaf28976e8e0a2ed
Malcome Fein <mfein90@gmail.com>
2018-08-03 20:22:13 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -15,16 +15,12 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
-  data.each do |show_season_number, contestants_data|
-    #binding.pry
-    contestants_data[show_season_number].each do |people|
+  data.each do |season, array|
+    array.each do |people|
       people.each do |k,v|
-        if v == "Winner"
-            full_name = people["name"]
-            return full_name.split(' ').first
-               binding.pry
-        end 
+        if v == occupation
+          return people["name"]
+        end
       end
     end
   end
